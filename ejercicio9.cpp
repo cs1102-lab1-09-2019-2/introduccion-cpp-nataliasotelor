4ac46146c199b1090329ae14730a442dd1fffa1a
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-08-25 20:20:42 -0500

    Ejercicio 9 arreglado

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-using std::cout;
+using namespace std;
 
 int main() {
-
-    int i = 1;
-    while (i <= 10) {
-        if (   ) {
-            //imprimir numeros pares verticalmente
+    int i=1;
+    for (int i=1; i<=10; i+=1)
+        if (i%2 == 0){
+            cout << i << endl;
         }
-
-    }
+    return 0;
 }
