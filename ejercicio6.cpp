258ea2d5592594170ec241528513ad112a176b2c
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-08-25 22:43:48 -0500

    Ejercicio 6 arreglado

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,13 +1,13 @@
-
-
+#include <iostream>
+#include <vector>
+using namespace std;
 int main() {
-    vector<vector<int>> v{{1, 2}, {3, 4}, {5, 6}};
+    vector<vector<int>> v{{1, 2}"/n", {3, 4}"/n", {5, 6}"/n"};
     for (auto a : v) {
         for (auto i : a) {
-            // imprimir i
+            cout<<i>>;
         }
-        // salto de linea
-    }
+    return 0   
 }
 
 
