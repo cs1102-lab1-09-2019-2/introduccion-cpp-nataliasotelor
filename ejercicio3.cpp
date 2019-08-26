5b4bd43ce4963cbbe1c05341c5bc19826f49d1b6
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-08-25 20:04:03 -0500

    ejercicio 3 arreglado

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-using std::cout;
+using namespace std;
 
 int main() {
-    for (int i = 0; i < ; i++) {
+    for (int i = 0; i <=9 ; i+=1) {
         //imprimir los 10 numeros [0-9]
+        cout<< i <<endl;
     }
 }
