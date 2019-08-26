132c4d5c82db0ee78000b6390fba493ad4050c59
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-08-25 21:20:11 -0500

    Ejercicio 5 arreglado

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
-using std::cout;
+#include<iostream>
+#include<vector>
+
+void print(std::vector <int> const &a) {
+   
+   for(int i=0; i < a.size(); i++)
+      std::cout << a[i] << ' ';
+}
 
 int main() {
-
-    vector<int> a {1, 2, 3, 4, 5};
-    for (auto i : a) {
-        // Imprimir los numeros
-    }
+   std::vector<int> a = {1,2,3,4,5};
+   print(a);
+   return 0;
 }
